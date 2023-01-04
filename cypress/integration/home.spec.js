75f6596767b6d4a7064eceb53d20c552e128190a
David Wang <18davidwang@gmail.com>
2023-01-03 17:37:54 -0800

    Adding Testing home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+  it("successfully loads", () => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
